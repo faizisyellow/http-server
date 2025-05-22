f0bf63e108dde5b51908ba9af54b629b03c7b8bf
faizisyellow <muhamatfaissalm@gmail.com>
2025-05-22 10:27:03 +0700

    ['return a file','read request body']

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
+	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -13,10 +16,16 @@ type Request struct {
 	Url    string
 	Proto  string
 	Header Header
+	Body   string
 }
 
 func main() {
-	ln, err := net.Listen("tcp", "0.0.0.0:4221")
+	ListenAndServe("4221")
+}
+
+func ListenAndServe(p string) {
+	port := fmt.Sprintf("0.0.0.0:%v", p)
+	ln, err := net.Listen("tcp", port)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -28,7 +37,7 @@ func main() {
 		return
 	}
 
-	go handleConnection(conn)
+	handleConnection(conn)
 }
 
 func handleConnection(conn net.Conn) {
@@ -54,6 +63,57 @@ func handleConnection(conn net.Conn) {
 		userAgent := req.Header["User-Agent"]
 
 		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %v\r\n\r\n%v", len(userAgent), userAgent)))
+	} else if strings.HasPrefix(req.Url, "/files/") {
+
+		if req.Method == "POST" {
+			filename, _ := strings.CutPrefix(req.Url, "/files/")
+			path := fmt.Sprintf("files/%v", filename)
+			f, err := os.Create(path)
+			if err != nil {
+				log.Fatal(err)
+				return
+			}
+
+			defer f.Close()
+
+			_, err = f.Write([]byte(req.Body))
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			conn.Write([]byte(fmt.Sprintf("HTTP/1.1 201 Created\r\n\r\n")))
+			return
+		}
+
+		dirpath, _ := strings.CutPrefix(req.Url, "/")
+		var fileName string
+
+		// if the directory empty it return dot
+		if filepath.Dir(dirpath) != "." {
+			paths, err := filepath.Glob(dirpath + ".*")
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			if len(paths) == 0 {
+				conn.Write([]byte("HTTP/1.1 404 OK\r\n\r\n"))
+				return
+			}
+
+			fileName = paths[0]
+		} else {
+			conn.Write([]byte("HTTP/1.1 404 OK\r\n\r\n"))
+			return
+		}
+
+		fileContent, err := os.ReadFile(fileName)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %v\r\n\r\n%v", len(string(fileContent)), string(fileContent))))
+
 	} else {
 
 		conn.Write([]byte("HTTP/1.1 404 OK\r\n\r\n"))
@@ -65,6 +125,7 @@ func handleConnection(conn net.Conn) {
 
 func ParseHttpRequest(data []byte) Request {
 	reqs := strings.Split(string(data), "\r\n")
+	body := strings.Split(string(data), "\r\n\r\n")[1]
 
 	// make alocates the variable to memory
 	var headers = make(Header)
@@ -86,5 +147,6 @@ func ParseHttpRequest(data []byte) Request {
 		Url:    strings.Split(reqs[0], " ")[1],
 		Proto:  strings.Split(reqs[0], " ")[2],
 		Header: headers,
+		Body:   body,
 	}
 }
